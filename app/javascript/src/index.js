79a14240d8e511c45db7fcbac1baecdee6e5a9d6
y-matsuda <y.matsuda@bizside.biz>
2023-03-17 13:05:25 +0900

    コメント追加

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -8,9 +8,21 @@ class BizQuest extends Phaser.Game {
   // コンストラクター
   constructor () {
     super(config);
+
+    // 起動画面
+    // この画面を見せて時間稼ぎをしている間に、ゲームの必要な画像やマップ情報を読み込んでいる。
     this.scene.add('boot', Boot);
+
+    // ゲームのメイン画面
+    // ゲーム世界や、街、洞窟など各シーンを切り替えている。
     this.scene.add('game', Game);
+
+    // ステータス画面
+    // スコアの表示など、ゲームの状態を表示するためのシーン。
+    // メイン画面の上に重ねて表示しているが、背景色が透明なので、メイン画面もちゃんと見えている。
     this.scene.add('ui', UI);
+
+    // 起動画面を見せるでゲーム開始
     this.scene.start('boot');
   }
 }
